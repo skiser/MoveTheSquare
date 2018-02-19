abd0921fcecdd92346e8cd60d55a904bd2292974
Samantha Kiser <skiser@ycp.edu>
2018-02-19 09:25:33 -0500

    Create circle methods like the square methods

---
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/model/Circle.java
@@ -0,0 +1,34 @@
+package edu.ycp.cs320.movethesquare.model;
+
+public class Circle {
+private double x, y, radius;
+	
+	public Circle() {
+		
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+
+}
